13009315b449d38891dea89e07b40d2858c54cc9
lovetonight <hoan.khoc@gmail.com>
2023-12-24 13:20:38 +0700

    m

--- process_image.py
+++ process_image.py
@@ -116,7 +116,7 @@ def get_output_image(path, index = 1):
                     pred = predict(roi)
                 case 3:
                     pred = predict_3(roi)      
-                case 3:
+                case 4:
                     pred = predict_knn(roi)                            
             print(pred)
             
